3fa4e3536da03ea689d35565c124f3402f672c8c
Saket <Saket@VAIO>
2019-04-01 00:03:55 +0530

    First Commit Uploading All files

---
+++ MyCode/src/demo/Calculator.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Calculator {
+	
+	public int add(int num1, int num2)
+	{
+		int r = num1+num2;
+		return r;
+	}
+	
+	public int sub(int num1, int num2)
+	{
+		int r = num1 - num2;
+		return r;
+	}
+
+
+}
